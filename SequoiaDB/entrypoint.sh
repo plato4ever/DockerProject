28029a955478988cc4fc9602750c688c755bee66
plato4ever <zhaoliping0736@gmail.com>
2017-03-07 21:20:45 +0800

    sdbcm

--- SequoiaDB/entrypoint.sh
+++ SequoiaDB/entrypoint.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-if [[ ! -f /opt/bootstarp.lock ]]; then
- 
-  #fix file permissions
-  chown -R root:root /opt/sequoiadb/database
-  chown -R root:root /opt/sequoiadb/conf
-  echo "Bootstrap finished."
-  touch /opt/bootstarp.lock
-
-else
-  echo "Already init."
-fi
-
-rm -f /etc/sysctl.conf
-mv /conf/sysctl.conf /etc/sysctl.conf
-/sbin/sysctl -p
-
-#通过环境变量设定容器运行
-if [ $DB_SYSTEM = "sdbw" ] ; then
-/etc/init.d/sdbcm start && /opt/sequoiadb/bin/sdbwsart -S 0.0.0.0:8080 && /usr/sbin/sshd -D
-else 
-/etc/init.d/sdbcm start && /usr/sbin/sshd -D
-fi
+systemctl enable sdbcm
+systemctl restart sdbcm
